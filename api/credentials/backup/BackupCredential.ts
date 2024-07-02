1a5f142eb88e41389243a3e4f44caf615ed3ec34
Florent Lecocq <lecocqflorent@outlook.fr>
2024-07-02 12:15:50 +0200

    🚧 Https

--- api/credentials/backup/BackupCredential.ts
+++ api/credentials/backup/BackupCredential.ts
@@ -3,7 +3,7 @@ import { JsonCredential, Request } from "@henrotaym/api-client";
 class BackupCredential extends JsonCredential {
   prepare(request: Request<any>): void {
     super.prepare(request);
-    request.setBaseUrl("backup.trustup.io/api");
+    request.setBaseUrl("https://backup.trustup.io/api");
   }
 }
 
