b8a11f33b13251b4a67fd537bdbb1a168f9e715a
Florent Lecocq <lecocqflorent@outlook.fr>
2024-07-02 12:08:56 +0200

    ✏️ Fix api url

--- api/credentials/backup/BackupCredential.ts
+++ api/credentials/backup/BackupCredential.ts
@@ -3,9 +3,8 @@ import { JsonCredential, Request } from "@henrotaym/api-client";
 class BackupCredential extends JsonCredential {
   prepare(request: Request<any>): void {
     super.prepare(request);
-    request
-      .setBaseUrl("http://localhost:48119/api");
+    request.setBaseUrl("backup.trustup.io/api");
   }
 }
 
-export default BackupCredential;  
+export default BackupCredential;
